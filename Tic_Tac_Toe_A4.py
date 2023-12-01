e927ff9f9e1f8b6324e8d931b83c9316484047a7
crai0263 <147572858+crai0263@users.noreply.github.com>
2023-12-01 22:59:29 +0000

    added code

--- Tic_Tac_Toe_A4.py
+++ Tic_Tac_Toe_A4.py
@@ -1,1 +1,56 @@
+#print board
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        if row != 2:
+            print("---------")
 
+def check_winner(board, player):
+    # Check rows
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+
+    # Check columns
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+
+    # Check diagonals
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+
+    return False
+
+#checking to see if the board is full
+def is_board_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+#running the game
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+
+        row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
+        col = int(input(f"Player {current_player}, enter column (0, 1, or 2): "))
+
+        if board[row][col] == ' ':
+            board[row][col] = current_player
+            if check_winner(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a tie!")
+                break
+            else:
+                current_player = 'O' if current_player == 'X' else 'X'
+        else:
+            print("Invalid move. Spot already taken. Enter a new space.")
+
+#running the game
+tic_tac_toe()
